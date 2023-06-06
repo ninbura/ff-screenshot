b3a5c05f0982609621d467a8f67a4bf924e98598
Gabriel Balaich <GJBalaich@afstores.com>
2023-06-06 13:34:58 -0600

    cleaned up code indendation

--- screenshot.ps1
+++ screenshot.ps1
@@ -64,129 +64,129 @@ function printConfig ($config) {
 }
 
 function generateDirectory ($defaultDirectory) {
-    Write-Host "Verifying directory exists..."
+  Write-Host "Verifying directory exists..."
 
-    if(!(Test-Path -Path $defaultDirectory)){
-        Write-Host "Directory does not exist, creating directory..."
+  if(!(Test-Path -Path $defaultDirectory)){
+    Write-Host "Directory does not exist, creating directory..."
 
-        New-Item -Path $defaultDirectory -ItemType "directory" -Force
+    New-Item -Path $defaultDirectory -ItemType "directory" -Force
 
-        if(Test-Path -Path $defaultDirectory){
-            Write-Host "`"$defaultDirectory`" has been created."
-        }
-        else{
-            Write-Host "`"$defaultDirectory`" could not be created, see log for details."
-            
-            exit
-        }
+    if(Test-Path -Path $defaultDirectory){
+      Write-Host "`"$defaultDirectory`" has been created."
     }
     else{
-        Write-Host "`"$defaultDirectory`" already exists, program will continue."
+      Write-Host "`"$defaultDirectory`" could not be created, see log for details."
+      
+      exit
     }
+  }
+  else{
+    Write-Host "`"$defaultDirectory`" already exists, program will continue."
+  }
 
-    Write-Host ""
+  Write-Host ""
 }
 
-Function generateArgumentList ($captureDevice, $resolution, $crop, $defaultDirectory, $outputFilename) {
-    Write-Host "Generating argument list..."
+function generateArgumentList ($captureDevice, $resolution, $crop, $defaultDirectory, $outputFilename) {
+  Write-Host "Generating argument list..."
 
-    $argumentList = @(
-      "-y",
-      "-loglevel", "error"
-      "-stats"
-      "-f", "dshow",
-      "-rtbufsize", "2147.48M",
-      "-i", "video=`"$captureDevice`"",
-      "-map", "0"
-    )
+  $argumentList = @(
+    "-y",
+    "-loglevel", "error"
+    "-stats"
+    "-f", "dshow",
+    "-rtbufsize", "2147.48M",
+    "-i", "video=`"$captureDevice`"",
+    "-map", "0"
+  )
 
-    if($crop){
-      $resolutionArray = $resolution.Split("x")
-      $resolution = [ordered]@{
-        width = $resolutionArray[0];
-        height = $resolutionArray[1];
-      }
-      $cropArray = $crop.Split("x")
-      $crop = [ordered]@{
-        left = $cropArray[0];
-        right = $cropArray[1];
-        top = $cropArray[2];
-        bottom = $cropArray[3];
-      }
-
-      $resolution.width = $resolution.width - $crop.left - $crop.right
-      $resolution.height = $resolution.height - $crop.top - $crop.bottom
-
-      $argumentList += "-vf", "`"crop=$($resolution.width):$($resolution.height):$($crop.left):$($crop.top)`""
+  if($crop){
+    $resolutionArray = $resolution.Split("x")
+    $resolution = [ordered]@{
+      width = $resolutionArray[0];
+      height = $resolutionArray[1];
+    }
+    $cropArray = $crop.Split("x")
+    $crop = [ordered]@{
+      left = $cropArray[0];
+      right = $cropArray[1];
+      top = $cropArray[2];
+      bottom = $cropArray[3];
     }
 
-    $argumentList += 
-        "-pix_fmt", "yuvj444p",
-        "-vframes", "1",
-        "-q:v", "2",
-        "`"$defaultDirectory\$outputFilename.jpeg`""
+    $resolution.width = $resolution.width - $crop.left - $crop.right
+    $resolution.height = $resolution.height - $crop.top - $crop.bottom
 
-    return $argumentList
+    $argumentList += "-vf", "`"crop=$($resolution.width):$($resolution.height):$($crop.left):$($crop.top)`""
+  }
+
+  $argumentList += 
+    "-pix_fmt", "yuvj444p",
+    "-vframes", "1",
+    "-q:v", "2",
+    "`"$defaultDirectory\$outputFilename.jpeg`""
+
+  return $argumentList
 }
 
 function printArgumentList($argumentList){
   Write-Host "Argument list:"
 
   for ($i = 0; $i -lt $argumentList.Length; $i++) {
-      if (
-          $argumentList[$i] -match "-" -and 
-          $i + 1 -lt $argumentList.Length -and
-          $argumentList[$i + 1] -notmatch "-"
-      ) {
-          Write-Host "$($argumentList[$i]) " -NoNewline
-      }
-      else {
-          Write-Host $argumentList[$i]
-      }
+    if (
+      $argumentList[$i] -match "-" -and 
+      $i + 1 -lt $argumentList.Length -and
+      $argumentList[$i + 1] -notmatch "-"
+    ) {
+      Write-Host "$($argumentList[$i]) " -NoNewline
+    }
+    else {
+      Write-Host $argumentList[$i]
+    }
   }
 
   Write-Host ""
 }
 
 function runFFmpegCommand ($argumentList, $outputFilePath) {
-    Write-Host "Generating screenshot..."
-    Start-Process "ffmpeg" -ArgumentList $argumentList -Wait -NoNewWindow
+  Write-Host "Generating screenshot..."
+  Start-Process "ffmpeg" -ArgumentList $argumentList -Wait -NoNewWindow
 
-    if(Test-Path -Path $outputFilePath){
-        Write-Host "`"$outputFilePath`" has been generated."
-    }
-    else{
-        Write-Host "Screenshot could not be generated, see above for more details..."
+  if(Test-Path -Path $outputFilePath){
+    Write-Host "`"$outputFilePath`" has been generated."
+  }
+  else{
+    Write-Host "Screenshot could not be generated, see above for more details..."
 
-        quit
-    }
-    
-    Write-Host ""
+    quit
+  }
+  
+  Write-Host ""
 }
 
 function copyToClipboard ($copyToClipboard, $outputFilePath) {
-    if($copyToClipboard){
-        Write-Host "Copying screenshot to clipboard..."
+  if($copyToClipboard){
+    Write-Host "Copying screenshot to clipboard..."
 
-        $screenshot = [System.Drawing.Image]::FromFile((Get-Item -Path $outputFilePath))
-        [System.Windows.Forms.Clipboard]::SetImage($screenshot)
-        $screenshot.Dispose()
+    $screenshot = [System.Drawing.Image]::FromFile((Get-Item -Path $outputFilePath))
+    [System.Windows.Forms.Clipboard]::SetImage($screenshot)
+    $screenshot.Dispose()
 
-        Write-Host "Picture has been copied to clipboard.`n"
-    }
+    Write-Host "Picture has been copied to clipboard.`n"
+  }
 }
 
 function deleteScreenshot ($saveToDirectory, $outputFilePath) {
-    if(!$saveToDirectory){
-        Write-Host "Save to directory is disabled, screenshot will now be deleted..."
+  if(!$saveToDirectory){
+    Write-Host "Save to directory is disabled, screenshot will now be deleted..."
 
-        Remove-Item -Path "$outputFilePath" -Force
+    Remove-Item -Path "$outputFilePath" -Force
 
-        Write-Host "$outputFilePath has been deleted.`n"
-    }
+    Write-Host "$outputFilePath has been deleted.`n"
+  }
 }
 
-try{
+try {
   Write-Host "Program is starting..."
   $argumentList = generateArgumentList $captureDevice $resolution $crop "C:\Users\$($env:UserName)\AppData\Local\Temp" $outputFilename
   $outputFilePath = $argumentList[$argumentList.Length - 1].Replace("`"", "")
