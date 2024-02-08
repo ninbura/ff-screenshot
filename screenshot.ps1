1dc26fe25b11e1a29ed2a3814667679fa412e4c9
ninbura <gabriel@ninbura.com>
2024-02-07 22:47:06 -0700

    added input format parameter

--- screenshot.ps1
+++ screenshot.ps1
@@ -4,7 +4,7 @@ param(
   [string]$saveToDirectory = "y",
   [string]$copyToClipboard = "y",
   [string]$captureDevice = "Game Capture 4K60 Pro MK.2",
-  [string]$colorFormat,
+  [string]$inputFormat,
   [string]$resolution, #= "3840x2160",
   [string]$crop, #= "718x723x0x0",
   [string]$outputFilename = [string](Get-Date -Format "yyyy-MM-dd HH-mm-ss"),
@@ -87,7 +87,7 @@ function generateArgumentList ($captureDevice, $resolution, $crop, $outputDirect
   )
 
   if($colorFormat){
-    $argumentList += "-pixel_format", $colorFormat
+    $argumentList += "-input_format", $colorFormat
   }
 
   $argumentList += "-i", "video=`"$captureDevice`"", "-map", "0"
